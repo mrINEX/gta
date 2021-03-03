605152c112941ff8f3f1146cbaa90d5c2bdd2954
mrinex <mrinex@mail.ru>
2021-03-04 01:10:52 +0300

    refactor: position start shot

--- src/components/guns/Pistol.jsx
+++ src/components/guns/Pistol.jsx
@@ -3,14 +3,14 @@ import SCHEMES from '../../constants/constants';
 
 const Pistol = (props) => {
   const { x, y, directionGun, isPressTrigger } = props;
-  const path = SCHEMES.schemePistol.replace(/%/g, '');
+  const path = SCHEMES.schemePistol3.replace(/%/g, '');
 
   if (isPressTrigger) {
     const ctx = document.getElementById('canvas').getContext('2d');
 
     ctx.save();
-    ctx.translate(x, y);
-    ctx.translate(78 * Math.cos((Math.PI / 180) * directionGun), 78 * Math.sin((Math.PI / 180) * directionGun) + 26);
+    ctx.translate(x + 10, y + 10);
+    ctx.translate(82 * Math.cos((Math.PI / 180) * directionGun), 82 * Math.sin((Math.PI / 180) * directionGun));
     // x = r*cos(fi)
     // y = r*sin(fi)
     ctx.rotate((Math.PI / 180) * directionGun);
@@ -19,9 +19,9 @@ const Pistol = (props) => {
   }
   
   return (
-    <g transform={`translate(${x}, ${y}) rotate(${directionGun})`}>
+    <g transform={`translate(${x + 10}, ${y + 10}) rotate(${directionGun})`}>
       <polyline id="laser"
-        points={`78,26 ${Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2))},26`}
+        points={`82,0 ${Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2))},0`}
         stroke="red" strokeWidth="1.6" strokeLinecap="round"/>
       <polygon id="pistol"
         points={path} stroke="black" strokeWidth="2"
